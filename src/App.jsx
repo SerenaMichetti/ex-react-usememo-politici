24af918fa54a450f164ad78b295e323d48b46c78
Serena <sere.michetti@icloud.com>
2026-06-23 20:16:14 +0200

    milestone 1 completato

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,35 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [politicians, setPoliticians] = useState([])
 
+  useEffect(() => {
+    fetch('http://localhost:3333/politicians')
+      .then(res => res.json())
+      .then(data => setPoliticians(data))
+      .catch(err => console.error(err))
+  }, [])
+
+  console.log(politicians)
   return (
     <>
-     
+      <h1>Lista Politici:</h1>
+      <div className='politicians-container'>
+        {politicians.map(politician => (
+          <div className='card' key={politician.id}>
+            <h2>Nome: {politician.name}</h2>
+            <img src={politician.image}></img>
+            <h2>Carica: {politician.position}</h2>
+            <p>Breve Biografia: {politician.biography}</p>
+          </div>
+        ))}
+
+      </div>
     </>
   )
 }
 
 export default App
+
+
