823c766120015a666f708f959f419c36386edc9d
Serena <sere.michetti@icloud.com>
2026-06-24 16:28:15 +0200

    milestone 2 completata

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useMemo} from 'react'
 import './App.css'
 
 function App() {
   const [politicians, setPoliticians] = useState([])
+  const [search, setSearch]= useState('')
 
   useEffect(() => {
     fetch('http://localhost:3333/politicians')
@@ -11,15 +12,26 @@ function App() {
       .catch(err => console.error(err))
   }, [])
 
-  console.log(politicians)
+ const filterResults= useMemo(()=>{
+  return politicians.filter(politician => {
+    const searchName= politician.name.toLowerCase().includes(search.toLocaleLowerCase())
+    const searchBio= politician.biography.toLocaleLowerCase().includes(search.toLocaleLowerCase())
+    return searchName || searchBio
+  })
+ },[politicians,search])
   return (
     <>
       <h1>Lista Politici:</h1>
+      <input type='text' 
+      placeholder='Cerca per nome o biografia' 
+      value={search}
+        onChange={e =>setSearch(e.target.value)}
+      ></input>
       <div className='politicians-container'>
-        {politicians.map(politician => (
+        {filterResults.map(politician => (
           <div className='card' key={politician.id}>
             <h2>Nome: {politician.name}</h2>
-            <img src={politician.image}></img>
+            <img src={politician.image} alt={politician.name}></img>
             <h2>Carica: {politician.position}</h2>
             <p>Breve Biografia: {politician.biography}</p>
           </div>
@@ -33,3 +45,12 @@ function App() {
 export default App
 
 
+// # 📌 Milestone 2: Implementare la ricerca ottimizzata
+// - Aggiungi un campo di ricerca (<input type="text">) sopra la lista dei politici.
+// - Permetti all’utente di filtrare i risultati in base a nome o biografia 
+// (se il testo cercato è incluso). 
+// Suggerimento: Creare un array derivato filtrato, che viene aggiornato solo quando
+//  cambia la lista di politici o il valore della ricerca.
+// - ❌ Non usare useEffect per aggiornare l’array filtrato.
+
+// - Obiettivo: Migliorare le prestazioni evitando ricalcoli inutili quando il valore della ricerca non cambia.
